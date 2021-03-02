9b4e08a7067f43999d05a2a1d489963f46b1b040
Wesley <wes.beck21@outlook.com>
2021-03-01 17:26:51 -0800

    commit tree 1189d283b0c583677b4a4ff53f0efac4cb386235

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(arr){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(arr[i]* (-1))
+  }
+  return newArr
+}
+
+function mapToNoChange(arr){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(arr[i])
+  }
+  return newArr
+}
+
+function mapToDouble(arr){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(arr[i]* 2)
+  }
+  return newArr
+}
+
+function mapToSquare(arr){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(arr[i]** 2)
+  }
+  return newArr
+}
+
+
+function reduceToTotal(arr, startingPoint=0) {
+  let total = startingPoint
+  for( let i = 0; i < arr.length; i++){
+    total = total + arr[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(arr){
+  for(let i = 0; i < arr.length; i++){
+    if (!arr[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(arr){
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i]) return true
+  }
+  return false
+}
